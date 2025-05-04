e7acff700c483dd2b63cac5c4c4554a739ac31d7
gharbim <gharbimalika039@gmail.com>
2025-05-04 01:43:55 +0100

    pages

--- angular-16/src/app/components/powerbi/overview/overview.component.ts
+++ angular-16/src/app/components/powerbi/overview/overview.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent {
-  powerBIUrl: string = 'https://app.powerbi.com/reportEmbed?reportId=0f13fd43-d27a-4e94-92bd-044c974c1141&autoAuth=true&ctid=604f1a96-cbe8-43f8-abbf-f8eaf5d85730';
+  powerBIUrl: string = 'https://app.powerbi.com/reportEmbed?reportId=0f13fd43-d27a-4e94-92bd-044c974c1141&autoAuth=true&ctid=604f1a96-cbe8-43f8-abbf-f8eaf5d85730&pageName=ReportSection2225be483213b82a04e3&navContentPaneEnabled=false&filterPaneEnabled=false';
 
 }
--- angular-16/src/app/components/powerbi/sales2/sales2.component.ts
+++ angular-16/src/app/components/powerbi/sales2/sales2.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./sales2.component.scss']
 })
 export class Sales2Component {
-  powerBiUrl = 'https://app.powerbi.com/reportEmbed?reportId=0f13fd43-d27a-4e94-92bd-044c974c1141&autoAuth=true&ctid=604f1a96-cbe8-43f8-abbf-f8eaf5d85730';
+  powerBiUrl = 'https://app.powerbi.com/reportEmbed?reportId=0f13fd43-d27a-4e94-92bd-044c974c1141&autoAuth=true&ctid=604f1a96-cbe8-43f8-abbf-f8eaf5d85730&pageName=ReportSection&navContentPaneEnabled=false&filterPaneEnabled=false';
 }
